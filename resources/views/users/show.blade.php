9a87fcd0d64e9db4e237044b0a18c8f13df7a851
luct2doo <t2doo@qq.com>
2020-11-09 15:53:15 +0000

    编辑个人资料

--- resources/views/users/show.blade.php
+++ resources/views/users/show.blade.php
@@ -7,7 +7,7 @@
   <div class="row">
 
     <div class="col-lg-3 col-md-3 hidden-sm hidden-xs user-info">
-      <div class="card ">
+      <div class="card">
         <img class="card-img-top" src="https://cdn.learnku.com/uploads/images/201709/20/1/PtDKbASVcz.png?imageView2/1/w/600/h/600" alt="{{ $user->name }}">
         <div class="card-body">
           <h5><strong>个人简介</strong></h5>
@@ -19,7 +19,7 @@
       </div>
     </div>
     <div class="col-lg-9 col-md-9 col-sm-12 col-xs-12">
-      <div class="card ">
+      <div class="card">
         <div class="card-body">
           <h1 class="mb-0" style="font-size:22px;">{{ $user->name }} <small>{{ $user->email }}</small></h1>
         </div>
@@ -27,7 +27,7 @@
       <hr>
 
       {{-- 用户发布的内容 --}}
-      <div class="card ">
+      <div class="card">
         <div class="card-body">
           暂无数据 ~_~
         </div>
